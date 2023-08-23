ea302630806cde8bf44ab465e554cc6955390bf5
a11arjunyadav <yadavarjun0312@gmail.com>
2023-08-23 15:01:44 +0530

    js array

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+var x = ["Arjun"];
+
+let y = [...x,'Yadav']; //... = Sprad opretor
+
+console.log(y);
+
+var n1 = ['Reilway']; //... = Sprad opretor
+let news = [...n1,"Union"];  //... = Sprad opretor
+const news2 = [...news,"railways"];  //... = Sprad opretor
+let news3 = [...news2,'minister'];
+var news4 = [...news3,`Ashwini`];
+let news5 = [...news4,"Vaishnaw"];  //... = Sprad opretor
+const news6 = [...news5,"announced"];
+var news7 = [...news6,"₹10"];  //... = Sprad opretor
+let news8 = [...news7,"lakh"];  //... = Sprad opretor
+const news9 = [...news8,"compensation"];
+var news10 = [...news9,"for"];
+let news11 = [...news10,"the"];  //... = Sprad opretor
+const news12 = [...news11,"kin"];
+var news13 = [...news12,"of"];
+let news14 = [...news13,"the"];  //... = Sprad opretor
+
+console.log(news14);
---
+++ js/script2.js
@@ -0,0 +1,9 @@
+
+//1. function define
+function getMyFullName(...args){  //Rest opretor
+    //function body
+    //statment
+    console.log(args);  //rest opretor
+}
+//2. function calline/Invoking
+getMyFullName("Arjun",'Yadav',27,5.8,["Riyansh",'krishna',`shreeyansh`]);
